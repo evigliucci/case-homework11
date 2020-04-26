35ec1e0f8aec1c5a5d36d3cbb8ef23ff7997275e
Erika Vigliucci <evigliucci@gmail.com>
2020-04-26 14:29:10 -0400

    Install app and dev dependencies add gitignore

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@
 // ===============================================================================
 
 var notesData = require("../db/db");
-
+import { v4 as uuidv4 } from 'uuid';
 
 // ===============================================================================
 // ROUTING
@@ -24,29 +24,26 @@ module.exports = function(app) {
 
     // API POST Requests
     // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
+    // When a user submits form data (a JSON object)
+    // Then the server saves the data to the notesData array)
     // ---------------------------------------------------------------------------
 
     app.post("/api/notes", function(req, res) {
         // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
         // It will do this by sending out the value "true" have a table
         // req.body is available since we're using the body parsing middleware
+        uuidv4();
+        console.log(uuidv4);
         notesData.push(req.body);
         res.json(true);
     });
 
-    // ---------------------------------------------------------------------------
-    // I added this below code so you could clear out the table while working with the functionality.
-    // Don"t worry about it!
-
-    app.post("/api/notes/:id", function(req, res) {
+    // Delete a note
+    app.delete("/api/notes/:id", function(req, res) {
         // Empty out the arrays of data
         //notesData.foreach(checkNoteID)
 
         //function checkNoteID()
-
+        res.send('Deleted Note [XYZ]')
     });
 };
