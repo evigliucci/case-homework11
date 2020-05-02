ae6d031386494547c49b0451f996ed504a4aab39
Erika Vigliucci <evigliucci@gmail.com>
2020-05-02 17:08:17 -0400

    Reverted logic - working app
    removed the call to store class as it wasn't needed.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,49 @@
 var path = require("path");
+var fs = require("fs");
 var router = require("express").Router();
-
+var store = require("../db/store");
 var notesData = require("../db/db");
+const uuid = require('uuid/v4');
 
 //api/notes
-router.get('/api/notes', function(req, res) {
-    //store.read
-    res.json(notesData);
+router.get('/notes', (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
 })
 
 //api/notes
-router.post("/api/notes", function(req, res) {
-    notesData.push(req.body);
+router.post("/notes", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuid()
+        }
+        notes.push(newNote);
+        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        });
+    });
 });
 
 // Delete a note
-router.delete("/api/notes/:id", function(req, res) {
-    res.send('Deleted Note [XYZ]')
+router.delete("/notes/:id", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const updatedNotes = notes.filter(note => note.id !== req.params.id);
+
+        fs.writeFile("./db/db.json", JSON.stringify(updatedNotes), (err) => {
+            if (err) throw err;
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        });
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-app.use("/", apiRoutes);
+app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 // Start the server on the port
