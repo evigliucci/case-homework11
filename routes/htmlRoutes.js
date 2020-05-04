fed369d726d8b98b3152494cf79fbbac9dd3f531
Erika Vigliucci <evigliucci@gmail.com>
2020-05-03 21:20:29 -0400

    HTML Routes code cleanup

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,16 @@
+// Include Dependencies 
 var path = require("path");
 var router = require("express").Router();
 
-// "/notes" responds with the notes.html file
-router.get("/notes", function(request, response) {
-    response.sendFile(path.join(__dirname, "../public/notes.html"));
+// Create route for /notes from public directory 
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 })
 
-// the default route, should redirect every other route request to index.html
-router.get("*", function(request, response) {
-    response.sendFile(path.join(__dirname, "../public/index.html"));
+// Create the default route. Redirect every other route request to index.html
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 })
 
+//Export Routes
 module.exports = router;
