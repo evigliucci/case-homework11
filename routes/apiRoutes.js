9fb1ad203262b04867e6d255945b4f8849480a4d
Erika Vigliucci <evigliucci@gmail.com>
2020-05-03 21:50:11 -0400

    Simplified Paths

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,11 +3,13 @@ const path = require("path");
 const fs = require("fs");
 const router = require("express").Router();
 const uuid = require('uuid/v4');
+const dbPath = path.join(__dirname, "../db/db.json");
+const notesPath = path.join(__dirname, "../public/notes.html");
 
 //READ NOTES
 // api/notes, Read notes for DB, check for errors, convert json and return the result. 
 router.get('/notes', (req, res) => {
-    fs.readFile("./db/db.json", (err, data) => {
+    fs.readFile(dbPath, (err, data) => {
         if (err) throw err;
         const notes = JSON.parse(data);
         res.json(notes);
@@ -17,7 +19,7 @@ router.get('/notes', (req, res) => {
 // ADD NEW NOTE
 // api/notes, Get exisitng notes, add new note to exisiting list of notes, add all updated notes to the DB. 
 router.post("/notes", (req, res) => {
-    fs.readFile("./db/db.json", (err, data) => {
+    fs.readFile(dbPath, (err, data) => {
         if (err) throw err;
         const notes = JSON.parse(data);
         const newNote = {
@@ -26,9 +28,9 @@ router.post("/notes", (req, res) => {
             id: uuid()
         }
         notes.push(newNote);
-        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+        fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
             if (err) throw err;
-            res.sendFile(path.join(__dirname, "../public/notes.html"));
+            res.sendFile(notesPath);
         });
     });
 });
@@ -36,14 +38,14 @@ router.post("/notes", (req, res) => {
 // DELETE NOTE
 // Get notes, check for errors, Filter out note based on ID from DB, send updated list to DB. 
 router.delete("/notes/:id", (req, res) => {
-    fs.readFile("./db/db.json", (err, data) => {
+    fs.readFile(dbPath, (err, data) => {
         if (err) throw err;
         const notes = JSON.parse(data);
         const updatedNotes = notes.filter(note => note.id !== req.params.id);
 
-        fs.writeFile("./db/db.json", JSON.stringify(updatedNotes), (err) => {
+        fs.writeFile(dbPath, JSON.stringify(updatedNotes), (err) => {
             if (err) throw err;
-            res.sendFile(path.join(__dirname, "../public/notes.html"));
+            res.sendFile(notesPath);
         });
     });
 });
